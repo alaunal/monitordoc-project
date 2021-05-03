3cb8613e78c43fd3f1f5b2f9453c7494c47f89dd
alaunal kauniyyah <alaunalkauniyyah3@gmail.com>
2021-05-04 01:21:02 +0700

    update page register & add patient

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,28 +1,32 @@
-import LazyLoad from "vanilla-lazyload";
+import LazyLoad from 'vanilla-lazyload'
 
 window.addEventListener('load', () => {
-  const modules = document.querySelectorAll('[data-module]');
+  const modules = document.querySelectorAll('[data-module]')
 
-  modules.forEach(node => {
-    let moduleName = node.dataset.module;
+  modules.forEach((node) => {
+    let moduleName = node.dataset.module
 
     switch (moduleName) {
-
       case 'datatables':
-        import('./modules/datatables')
-        .then((module) => {
-          module.default(node);
-        });
-        break;
+        import('./modules/datatables').then((module) => {
+          module.default(node)
+        })
+        break
+
+      case 'tabs':
+        import('./modules/tabs').then((module) => {
+          module.default(node)
+        })
+        break
 
       default:
-        console.log('module not found!');
-        break;
+        console.log('module not found!')
+        break
     }
-  });
+  })
 
   // -- LazyLoad Init
   new LazyLoad({
-    elements_selector: '.lazy'
-  }).update();
-});
+    elements_selector: '.lazy',
+  }).update()
+})
---
+++ src/assets/js/modules/tabs.js
@@ -0,0 +1,39 @@
+const tabs = (params) => {
+
+    let idTarget = params.dataset.target;
+    let navTabs = params.querySelectorAll('.tabs-nav');
+
+    navTabs.forEach(element => {
+
+
+        element.addEventListener('click', (e) => {
+
+            e.preventDefault();
+
+            let activeElement = e.target.classList.contains('active');
+
+            if (!activeElement) {
+                let targetNav = e.target;
+                let idTab = targetNav.dataset.tab;
+
+                let contentTabsActive = document.querySelector(`${idTarget}>${idTab}`);
+                let contentTabs = document.querySelectorAll(`${idTarget} .pane`);
+
+                [].forEach.call(navTabs, function(el) {
+                    el.classList.remove("active");
+                });
+
+                [].forEach.call(contentTabs, function(el) {
+                    el.classList.remove("active");
+                });
+
+                contentTabsActive.classList.add('active');
+                targetNav.classList.add('active');
+            }
+        });
+    });
+
+    return true;
+};
+
+export default tabs;
